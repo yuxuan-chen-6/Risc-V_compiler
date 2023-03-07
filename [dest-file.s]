1e24921b954eccb09159369bdbf339393388ba81
yuxuan-chen-6 <69693952+yuxuan-chen-6@users.noreply.github.com>
2023-03-07 14:58:57 +0000

    Add files via upload

---
+++ [dest-file.s]
@@ -0,0 +1,8 @@
+.text
+.globl f
+
+f:
+addi  t0, zero, 0
+addi  t0, t0,   5
+add   a0, zero, t0
+ret
---
+++ git_update.sh
@@ -0,0 +1,5 @@
+git add .
+echo "Please enter commit text: "
+read input
+git commit -m"$input"
+git push
